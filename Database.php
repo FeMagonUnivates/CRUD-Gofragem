f718e803a2cc1c8ecd35349b174241df70226409
FERNANDO MAGON WEIZENMANN <fernando.weizenmann@acad.univates>
2026-06-02 21:34:00 -0300

    atualização do env

--- Database.php
+++ Database.php
@@ -1,19 +1,21 @@
 <?php
 
+require_once __DIR__ . '/Env.php';
+
 class DataBase {
 
     public $connection;
 
     public function __construct() {
-        $host       = "localhost";
-        $porta      = "5432";
-        $database   = "Gofragem";
-        $usuario    = "postgres";
-        $senha      = "postgres";
+        $host       = getenv('DB_HOST');
+        $port       = getenv('DB_PORT');
+        $database   = getenv('DB_NAME');
+        $user       = getenv('DB_USER');
+        $pass       = getenv('DB_PASS');
 
-        $dsn = "pgsql: host=$host; port=$porta dbname=$database";
+        $dsn = "pgsql: host=$host; port=$port dbname=$database";
 
-        $this->connection = new PDO($dsn, $usuario, $senha);
+        $this->connection = new PDO($dsn, $user, $pass);
         $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
